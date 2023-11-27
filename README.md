4797dc060b97bed36b233d3a90e12b850513866f
Ygor Nascimento <ygor.nascimento@outlook.com>
2023-11-27 12:26:37 -0300

    Readme file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# SwiftUIByExample-05-ViewLayout
+# Hacking With Swift - SwiftUI By Example Book - Chapter 05 - View Layout
+Layout sizes, priorities, and spacing.
---
+++ SwiftUIByExample-05-ViewLayout/SwiftUIByExample-05-ViewLayout/SwiftUIByExample_05_ViewLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIByExample_05_ViewLayoutApp.swift
+//  SwiftUIByExample-05-ViewLayout
+//
+//  Created by Ygor Nascimento on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIByExample_05_ViewLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
